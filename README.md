912ad955f7ede6ebb0b59596893e0b89a63f8b9d
johnston9 <davejohnston9@gmail.com>
2021-02-26 20:27:14 +0000

    Added security check if session[user] in all app functions

--- README.md
+++ README.md
@@ -1,45 +1,26 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+<h1 align="center">Shot Caller - Movie Production Website</h1>
 
-Welcome johnston9,
+[View the project live here.](https://ms3-shot-caller.herokuapp.com/)
 
-This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Gitpod and the extensions we use.
+A web application for film production, allowing measages, images, scrips and shotlists to be sent and read through the different
+departments involved from pre-production to post. The easy to use and streamlined app provides a simple and enjoyable
+means of departmental communication and facilitates all areas of film production. Once established in the area of messaging the app's future development
+will expand into more specific areas of production.
+ 
 
-## Gitpod Reminders
 
-To run a frontend (HTML, CSS, Javascript only) application in Gitpod, in the terminal, type:
+<h2 align="center"><img src="documentation/readme-images/user16.png"></h2>
 
-`python3 -m http.server`
 
-A blue button should appear to click: *Make Public*,
+## User Experience (UX)
 
-Another blue button should appear to click: *Open Browser*.
+The business goals are to establish the app as a reputable and enjoyable tool in 
+the industry firstly in terms of the simple communication of messages, ideas and
+images for each department. The goal is for this to lead to further application 
+development in film production aiding in storyboarding, shotlist imaging and all
+pre-visualization areas.
 
-To run a backend Python file, type `python3 app.py`, if your Python file is named `app.py` of course.
+For the user the goal of the app is to enhance and facilitate film production.
+It will allow a simple and enjoyable means on departmental and inter-departmental
+communication. It is streamlined for the exchange of messages by specific areas.
 
-A blue button should appear to click: *Make Public*,
-
-Another blue button should appear to click: *Open Browser*.
-
-In Gitpod you have superuser security privileges by default. Therefore you do not need to use the `sudo` (superuser do) command in the bash terminal in any of the lessons.
-
-## Updates Since The Instructional Video
-
-We continually tweak and adjust this template to help give you the best experience. Here is the version history:
-
-**October 21 2020:** Versions of the HTMLHint, Prettier, Bootstrap4 CDN and Auto Close extensions updated. The Python extension needs to stay the same version for now.
-
-**October 08 2020:** Additional large Gitpod files (`core.mongo*` and `core.python*`) are now hidden in the Explorer, and have been added to the `.gitignore` by default.
-
-**September 22 2020:** Gitpod occasionally creates large `core.Microsoft` files. These are now hidden in the Explorer. A `.gitignore` file has been created to make sure these files will not be committed, along with other common files.
-
-**April 16 2020:** The template now automatically installs MySQL instead of relying on the Gitpod MySQL image. The message about a Python linter not being installed has been dealt with, and the set-up files are now hidden in the Gitpod file explorer.
-
-**April 13 2020:** Added the _Prettier_ code beautifier extension instead of the code formatter built-in to Gitpod.
-
-**February 2020:** The initialisation files now _do not_ auto-delete. They will remain in your project. You can safely ignore them. They just make sure that your workspace is configured correctly each time you open it. It will also prevent the Gitpod configuration popup from appearing.
-
-**December 2019:** Added Eventyret's Bootstrap 4 extension. Type `!bscdn` in a HTML file to add the Bootstrap boilerplate. Check out the <a href="https://github.com/Eventyret/vscode-bcdn" target="_blank">README.md file at the official repo</a> for more options.
-
---------
-
-Happy coding!
--- app.py
+++ app.py
@@ -82,210 +82,225 @@ def register():
 
 @app.route("/user_home/<username>", methods=["GET", "POST"])
 def user_home(username):
-    script = list(mongo.db.latest_script.find())
-    depts = list(mongo.db.depts.find())
-    shotlist = list(mongo.db.shotlist.find())
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
     if session["user"]:
-        return render_template(
-            "user_home.html", username=username,
-            script=script, shotlist=shotlist, depts=depts)
+        script = list(mongo.db.latest_script.find())
+        depts = list(mongo.db.depts.find())
+        shotlist = list(mongo.db.shotlist.find())
+        username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+        if session["user"]:
+            return render_template(
+                "user_home.html", username=username,
+                script=script, shotlist=shotlist, depts=depts)
 
-    return redirect(url_for("login"))
+        return redirect(url_for("login"))
 
  
 @app.route("/get_depts")
 def get_depts():
-    depts = mongo.db.depts.find()
-    return render_template("depts.html", depts=depts)
+    if session["user"]:
+        depts = mongo.db.depts.find()
+        return render_template("depts.html", depts=depts)
 
 
 @app.route("/get_dep/<dep>", methods=["GET", "POST"])
 def get_dep(dep):
-    dep = dep
-    day = datetime.datetime.now()
-    today = day.strftime("%d %B, %Y")
-    depart = list(mongo.db[dep].find({"date": today}))
-    if request.method == "POST":
-        date = request.form.get("date")
-        depart = list(mongo.db[dep].find(
-            {"date": date}))
+    if session["user"]:
+        dep = dep
+        day = datetime.datetime.now()
+        today = day.strftime("%d %B, %Y")
+        depart = list(mongo.db[dep].find({"date": today}))
+        if request.method == "POST":
+            date = request.form.get("date")
+            depart = list(mongo.db[dep].find(
+                {"date": date}))
 
+            return render_template(
+                "dep.html", dep=dep, depart=depart, date=date, day=date)
         return render_template(
-            "dep.html", dep=dep, depart=depart, date=date, day=date)
-    return render_template(
-        "dep.html", dep=dep, depart=depart, date=today, day="TODAY")
+            "dep.html", dep=dep, depart=depart, date=today, day="TODAY")
 
 
 @app.route("/get_poster/<dep>", methods=["GET", "POST"])
 def get_poster(dep):
-    dep = dep
-    if request.method == "POST":
-        poster = request.form.get("poster").lower()
-        depart = list(mongo.db[dep].find(
-            {"poster": poster}))
+    if session["user"]:
+        dep = dep
+        if request.method == "POST":
+            poster = request.form.get("poster").lower()
+            depart = list(mongo.db[dep].find(
+                {"poster": poster}))
 
+            return render_template(
+                "dep-poster.html", dep=dep, depart=depart, day=poster)
         return render_template(
-            "dep-poster.html", dep=dep, depart=depart, day=poster)
-    return render_template(
-        "dep-poster.html", dep=dep)
+            "dep-poster.html", dep=dep)
 
 
 @app.route("/get_all/<dep>", methods=["GET", "POST"])
 def get_all(dep):
-    dep = dep
-    depart = list(mongo.db[dep].find())
+    if session["user"]:
+        dep = dep
+        depart = list(mongo.db[dep].find())
 
-    return render_template(
-        "dep.html", dep=dep, depart=depart, day="All Messages")
+        return render_template(
+            "dep.html", dep=dep, depart=depart, day="All Messages")
 
 
 @app.route("/find_all/<dep>", methods=["GET", "POST"])
 def find_all(dep):
-    dep = dep
-    depart = list(mongo.db[dep].find())
+    if session["user"]:
+        dep = dep
+        depart = list(mongo.db[dep].find())
 
-    return render_template(
-        "dep-poster.html", dep=dep, depart=depart, day="All Messages")
+        return render_template(
+            "dep-poster.html", dep=dep, depart=depart, day="All Messages")
 
 
 @app.route("/get_image/", methods=["GET", "POST"])
 def get_image():
-    if request.method == "POST":
-        image = request.form.get("image")
-        images = list(mongo.db.images.find({"$text": {"$search": image}}))
-        return render_template("images.html", images=images)
+    if session["user"]:
+        if request.method == "POST":
+            image = request.form.get("image")
+            images = list(mongo.db.images.find({"$text": {"$search": image}}))
+            return render_template("images.html", images=images)
 
-    images = list(mongo.db.images.find({"image_name": "cat"}))
-    return render_template("images.html", images=images)
+        images = list(mongo.db.images.find({"image_name": "cat"}))
+        return render_template("images.html", images=images)
 
 
 @app.route("/logout")
 def logout():
-    # delete user session cookie
-    flash("Logout successful")
-    session.pop("user")
-    return redirect(url_for("login"))
+    if session["user"]:
+        # delete user session cookie
+        flash("Logout successful")
+        session.pop("user")
+        return redirect(url_for("login"))
 
 
 @app.route("/add_message", methods=["GET", "POST"])
 def add_message():
-    if request.method == "POST":
-        mes_is_priority = "on" if request.form.get("is_priority") else "off"
-        day = datetime.datetime.now()
-        mes_date = day.strftime("%d %B, %Y")
-        mes_dept = request.form.get("department_name")
-        user = mongo.db.users.find_one({"username": session["user"]})
-        first = user["firstname"]
-        last = user["lastname"]
-        username = session["user"]
-        mes_poster = f"{first} {last}"
-        mes_job = user["job_title"]
-        message = {
-            "dept_name": mes_dept,
-            "date": mes_date,
-            "poster": mes_poster,
-            "username": username,
-            "job_title": mes_job,
-            "subject": request.form.get("subject"),
-            "message": request.form.get("message"),
-            "image_src": request.form.get("image_src"),
-            "image_name": request.form.get("image_name"),
-            "is_priority": mes_is_priority,
-        }
-        mongo.db[mes_dept].insert_one(message)
-        flash("Message Added")
-        return redirect(url_for("user_home", username=session["user"]))
+    if session["user"]:
+        if request.method == "POST":
+            mes_is_priority = "on" if request.form.get("is_priority") else "off"
+            day = datetime.datetime.now()
+            mes_date = day.strftime("%d %B, %Y")
+            mes_dept = request.form.get("department_name")
+            user = mongo.db.users.find_one({"username": session["user"]})
+            first = user["firstname"]
+            last = user["lastname"]
+            username = session["user"]
+            mes_poster = f"{first} {last}"
+            mes_job = user["job_title"]
+            message = {
+                "dept_name": mes_dept,
+                "date": mes_date,
+                "poster": mes_poster,
+                "username": username,
+                "job_title": mes_job,
+                "subject": request.form.get("subject"),
+                "message": request.form.get("message"),
+                "image_src": request.form.get("image_src"),
+                "image_name": request.form.get("image_name"),
+                "is_priority": mes_is_priority,
+            }
+            mongo.db[mes_dept].insert_one(message)
+            flash("Message Added")
+            return redirect(url_for("user_home", username=session["user"]))
 
-    depts = mongo.db.depts.find().sort("dept_name", 1)
-    return render_template("add_message.html", depts=depts)
+        depts = mongo.db.depts.find().sort("dept_name", 1)
+        return render_template("add_message.html", depts=depts)
 
 
 @app.route("/edit_message/<message_id>/<depart>", methods=["GET", "POST"])
 def edit_message(message_id, depart):
-    if request.method == "POST":
-        mes_is_priority = "on" if request.form.get("is_priority") else "off"
-        day = datetime.datetime.now()
-        mes_date = day.strftime("%d %B, %Y")
-        mes_dept = request.form.get("department_name")
-        user = mongo.db.users.find_one({"username": session["user"]})
-        username = session["user"]
-        first = user["firstname"]
-        last = user["lastname"]
-        mes_poster = f"{first} {last}"
-        mes_job = user["job_title"]
-        edit = {
-            "dept_name": mes_dept,
-            "date": mes_date,
-            "poster": mes_poster,
-            "username": username,
-            "job_title": mes_job,
-            "subject": request.form.get("subject"),
-            "message": request.form.get("message"),
-            "image_src": request.form.get("image_src"),
-            "image_name": request.form.get("image_name"),
-            "is_priority": mes_is_priority,
-        }
-        mongo.db[mes_dept].update({"_id": ObjectId(message_id)}, edit)
-        flash("Message Updated")
-        return redirect(url_for("get_dep", dep=mes_dept))
+    if session["user"]:
+        if request.method == "POST":
+            mes_is_priority = "on" if request.form.get(
+                "is_priority") else "off"
+            day = datetime.datetime.now()
+            mes_date = day.strftime("%d %B, %Y")
+            mes_dept = request.form.get("department_name")
+            user = mongo.db.users.find_one({"username": session["user"]})
+            username = session["user"]
+            first = user["firstname"]
+            last = user["lastname"]
+            mes_poster = f"{first} {last}"
+            mes_job = user["job_title"]
+            edit = {
+                "dept_name": mes_dept,
+                "date": mes_date,
+                "poster": mes_poster,
+                "username": username,
+                "job_title": mes_job,
+                "subject": request.form.get("subject"),
+                "message": request.form.get("message"),
+                "image_src": request.form.get("image_src"),
+                "image_name": request.form.get("image_name"),
+                "is_priority": mes_is_priority,
+            }
+            mongo.db[mes_dept].update({"_id": ObjectId(message_id)}, edit)
+            flash("Message Updated")
+            return redirect(url_for("get_dep", dep=mes_dept))
 
-    message = mongo.db[depart].find_one({"_id": ObjectId(message_id)})
-    depts = mongo.db.depts.find().sort("dept_name", 1)
-    return render_template("edit_message.html", message=message, depts=depts)
+        message = mongo.db[depart].find_one({"_id": ObjectId(message_id)})
+        depts = mongo.db.depts.find().sort("dept_name", 1)
+        return render_template(
+            "edit_message.html", message=message, depts=depts)
 
 
 @app.route("/delete_message/<message_id>/<depart>")
 def delete_message(message_id, depart):
-    mongo.db[depart].remove({"_id": ObjectId(message_id)})
-    flash("Message Deleted")
-    return redirect(url_for("get_dep", dep=depart))
+    if session["user"]:
+        mongo.db[depart].remove({"_id": ObjectId(message_id)})
+        flash("Message Deleted")
+        return redirect(url_for("get_dep", dep=depart))
 
 
 @app.route("/add_script/<script_id>", methods=["GET", "POST"])
 def add_script(script_id):
-    script = list(mongo.db.latest_script.find())
-    if request.method == "POST":
-        latest = {
-            "script": request.form.get("script_name")
-        }
-        mongo.db.latest_script.update(
-            {"_id": ObjectId(script_id)}, latest)
-        flash("Script Successfully Updated")
-        return redirect(url_for("user_home", username=session["user"]))
+    if session["user"] == "admin":
+        script = list(mongo.db.latest_script.find())
+        if request.method == "POST":
+            latest = {
+                "script": request.form.get("script_name")
+            }
+            mongo.db.latest_script.update(
+                {"_id": ObjectId(script_id)}, latest)
+            flash("Script Successfully Updated")
+            return redirect(url_for("user_home", username=session["user"]))
 
-    return render_template("add_script.html", script=script)
+        return render_template("add_script.html", script=script)
 
 
 @app.route("/add_shot", methods=["GET", "POST"])
 def add_shot():
-    if request.method == "POST":
-        newshot = {
-            "shotlist": request.form.get("shot_name")
-        }
-        mongo.db.shotlist.update(
-            {"_id": ObjectId("6029b7f80febec6e0f942fcb")}, newshot)
-        flash("Shotlist Successfully Updated")
-        return redirect(url_for("user_home", username=session["user"]))
+    if session["user"] == "admin":
+        if request.method == "POST":
+            newshot = {
+                "shotlist": request.form.get("shot_name")
+            }
+            mongo.db.shotlist.update(
+                {"_id": ObjectId("6029b7f80febec6e0f942fcb")}, newshot)
+            flash("Shotlist Successfully Updated")
+            return redirect(url_for("user_home", username=session["user"]))
 
-    return render_template("add_shotlist.html")
+        return render_template("add_shotlist.html")
 
 
 @app.route("/add_image", methods=["GET", "POST"])
 def add_image():
-    if request.method == "POST":
-        new_image = {
-            "image_name": request.form.get("image_name"),
-            "image_des": request.form.get("image_des"),
-            "image_src": request.form.get("image_src")
-        }
-        mongo.db.images.insert_one(new_image)
-        flash("Image Added")
-        return redirect(url_for("user_home", username=session["user"]))
+    if session["user"] == "admin":
+        if request.method == "POST":
+            new_image = {
+                "image_name": request.form.get("image_name"),
+                "image_des": request.form.get("image_des"),
+                "image_src": request.form.get("image_src")
+            }
+            mongo.db.images.insert_one(new_image)
+            flash("Image Added")
+            return redirect(url_for("user_home", username=session["user"]))
 
-    return render_template("add_image.html")
-
+        return render_template("add_image.html")
 
 
 if __name__ == "__main__":
