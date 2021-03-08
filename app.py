db651b37b03d8773e204131255b860983c7498f9
johnston9 <davejohnston9@gmail.com>
2021-03-08 20:46:35 +0000

    Testing brute force attacks

--- app.py
+++ app.py
@@ -442,4 +442,4 @@ def delete_image(image_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
