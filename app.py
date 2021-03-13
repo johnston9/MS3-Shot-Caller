9deac673da824486a9ca92bbbfc81cb562a82d40
johnston9 <davejohnston9@gmail.com>
2021-03-13 01:11:47 +0000

    Added Docstring to app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ mongo = PyMongo(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Render 404 template.
+    """Render 404 page.
 
-    When an error message occur render the 404.html template.
+    When an error message occur render the 404.html page.
 
     :param 404: the error code
     :param e: the error code
@@ -33,7 +33,7 @@ def page_not_found(e):
     """
 
     if session["user"]:
-        # note that we set the 404 status explicitly
+        # render 404 page
         return render_template('404.html'), 404
 
 
@@ -43,8 +43,8 @@ def login():
     """Log the user in.
 
     On Get renders the login page.
-    On Post verifies the user's password and if valid 
-    sets the user as the sesson user 
+    On Post verifies the user's password and if valid
+    sets the user as the sesson user
     and renders the User Base page.
 
     :return: user_home.html
@@ -60,19 +60,24 @@ def login():
             # use Wertzeug to check stored password matches user input
             if check_password_hash(old_user["password"], request.form.get(
                     "password")):
+                # set user as sesson user
                 session["user"] = request.form.get("username").lower()
+                # render User Base page
                 return redirect(url_for(
                     "user_home", username=session["user"]))
             else:
                 # invalid password match
                 flash("Entry Incorrect")
+                # render Login page
                 return redirect(url_for("login"))
 
         else:
             # username doesn't exist
             flash("Entry Incorrect")
+            # render Login page
             return redirect(url_for("login"))
 
+    # render Login page
     return render_template("login.html")
 
 
@@ -93,7 +98,7 @@ def register():
     if request.method == "POST":
         if request.form.get("key").lower() == app.register_key:
 
-            # check to see if username is taken
+            # check Mongo to see if username is taken
             old_user = mongo.db.users.find_one(
                 {"username": request.form.get("username").lower()})
 
@@ -110,18 +115,18 @@ def register():
                 "password": generate_password_hash(
                     request.form.get("password"))
             }
-            # insert new user dict into Mongo
+            # insert new user document into Mongo
             mongo.db.users.insert_one(new_user)
 
             # set session cookie to this username
             session["user"] = request.form.get("username").lower()
             flash("Welcome aboard, {}".format(
                             request.form.get("username")))
-            # render user base page
+            # render User Base page
             return redirect(url_for("user_home", username=session["user"]))
         else:
             flash("Invalid Key")
-    # render register page
+    # render Register page
     return render_template("register.html")
 
 
@@ -142,13 +147,14 @@ def user_home(username):
     """
 
     if session["user"]:
-        # get data from Mongo
+        # get depts, latest_script, shotlist and users 
+        # collections data from Mongo
         script = list(mongo.db.latest_script.find())
         depts = list(mongo.db.depts.find())
         shotlist = list(mongo.db.shotlist.find())
         username = mongo.db.users.find_one(
             {"username": session["user"]})["username"]
-        # render the user_home template
+        # render the User Base page
         return render_template(
             "user_home.html", username=username,
             script=script, shotlist=shotlist, depts=depts)
@@ -166,8 +172,9 @@ def get_depts():
     """
 
     if session["user"]:
+        # get depts collection's data from Mongo
         depts = mongo.db.depts.find()
-        # render the depts template
+        # render the Departmentts page
         return render_template("depts.html", depts=depts)
 
 
@@ -175,7 +182,7 @@ def get_depts():
 def get_pro():
     """Renders the Production latest messages template page.
 
-    Renders the Production latest messages template page with the latest
+    Renders the Production Latest Messages page with the latest
     messages showing for the Production department
     getting the department's messages from Mongo.
 
@@ -186,10 +193,10 @@ def get_pro():
     if session["user"]:
         day = datetime.datetime.now()
         today = day.strftime("%d %B, %Y")
-        # get data from Mongo
+        # get production collection's data from Mongo
         depart = list(mongo.db.production.find({"date": today}))
 
-        # render the Production latest messages template
+        # render the Production Latest messages page
         return render_template(
             "pro.html", dep="Today's Production Updates", depart=depart,
             date=today, day="TODAY")
@@ -197,9 +204,9 @@ def get_pro():
 
 @app.route("/get_dep/<dep>", methods=["GET", "POST"])
 def get_dep(dep):
-    """Renders the Department messages by Date page.
+    """Renders the Department Messages by Date page.
 
-    On Get renders the Department messages by Date page with that day's
+    On Get renders the Department Messages by Date page with that day's
     messages showing for each department
     depending on which department name is passed as a parameter
     getting that department's messages from Mongo.
@@ -219,27 +226,29 @@ def get_dep(dep):
         dep = dep
         day = datetime.datetime.now()
         today = day.strftime("%d %B, %Y")
-        # get data from Mongo
+        # get selected department collection's documents posted 
+        # today from Mongo
         depart = list(mongo.db[dep].find({"date": today}))
         if request.method == "POST":
             date = request.form.get("date")
-            # get data from Mongo
+            # get selected department collection's documents 
+            # with this date from Mongo
             depart = list(mongo.db[dep].find(
                 {"date": date}))
 
-            # render the Department template
+            # render the selected Department page
             return render_template(
                 "dep.html", dep=dep, depart=depart, date=date, day=date)
-        # render the Department template
+        # render the selected Department page
         return render_template(
             "dep.html", dep=dep, depart=depart, date=today, day="TODAY")
 
 
 @app.route("/get_poster/<dep>", methods=["GET", "POST"])
 def get_poster(dep):
-    """Renders the Department messages by Poster page.
+    """Renders the Department Messages by Poster page.
 
-    On Get renders the Department messages by Poster page with the latest
+    On Get renders the Department Messages by Poster page with the latest
     messages showing for each department
     depending on which department name is passed as a parameter
     getting that department's messages from Mongo.
@@ -256,19 +265,20 @@ def get_poster(dep):
     """
 
     if session["user"]:
-        # get data from Mongo
+        # get selected department collection's last 10 documents from Mongo
         depart = list(mongo.db[dep].find().sort('date', -1).limit(10))
         dep = dep
         if request.method == "POST":
             poster = request.form.get("poster").lower()
-            # get data from Mongo
+            # get selected department collection's documents by this 
+            # poster from Mongo
             depart = list(mongo.db[dep].find(
                 {"poster": poster}))
 
-            # render the Department template
+            # render the Department page
             return render_template(
                 "dep-poster.html", dep=dep, depart=depart, day=poster)
-        # render the Department template
+        # render the Department page
         return render_template(
             "dep-poster.html", dep=dep, depart=depart, day="Latest")
 
@@ -289,9 +299,10 @@ def get_all(dep):
 
     if session["user"]:
         dep = dep
-        # get data from Mongo
+        # get selected department collection's data from Mongo
         depart = list(mongo.db[dep].find())
 
+        # Render the Department by Date page
         return render_template(
             "dep.html", dep=dep, depart=depart, day="All Messages")
 
@@ -312,9 +323,10 @@ def find_all(dep):
 
     if session["user"]:
         dep = dep
-        # get data from Mongo
+        # get selected department collection's data from Mongo
         depart = list(mongo.db[dep].find())
 
+        # Render the Department by Poster page
         return render_template(
             "dep-poster.html", dep=dep, depart=depart, day="All Messages")
 
@@ -331,8 +343,9 @@ def all_images():
     """
 
     if session["user"]:
-        # get data from Mongo
+        # get images collection's data from Mongo
         images = list(mongo.db.images.find())
+        # Render the Images page
         return render_template("images.html", images=images)
 
 
@@ -352,12 +365,14 @@ def get_image():
     if session["user"]:
         if request.method == "POST":
             image = request.form.get("image")
-            # get data from Mongo
+            # get images collection's documents matching search queries from Mongo
             images = list(mongo.db.images.find({"$text": {"$search": image}}))
+            # Render the Images page
             return render_template("images.html", images=images)
 
-        # get data from Mongo
+        # get image with name "windowlight" from images collection from Mongo
         images = list(mongo.db.images.find({"image_name": "windowlight"}))
+        # Render the Images page
         return render_template("images.html", images=images)
 
 
@@ -376,6 +391,7 @@ def logout():
         # delete user session cookie
         flash("Logout successful")
         session.pop("user")
+        # Render the Login page
         return redirect(url_for("login"))
 
 
@@ -400,7 +416,7 @@ def add_message():
             # get date from datetime
             day = datetime.datetime.now()
             mes_date = day.strftime("%d %B, %Y")
-            # get data from Mongo
+            # get users collection data from Mongo
             user = mongo.db.users.find_one({"username": session["user"]})
             first = user["firstname"]
             last = user["lastname"]
@@ -421,12 +437,14 @@ def add_message():
                 "image_src": request.form.get("image_src"),
                 "image_name": request.form.get("image_name"),
             }
-            # insert data to Mongo
+            # insert document into selected department collection in Mongo
             mongo.db[mes_dept].insert_one(message)
             flash("Message Added")
+            # Render the User Home page
             return redirect(url_for("user_home", username=session["user"]))
 
         depts = mongo.db.depts.find().sort("dept_name", 1)
+        # Render the Add Messages page
         return render_template("add_message.html", depts=depts)
 
 
@@ -460,7 +478,7 @@ def edit_message(message_id, depart, user):
             day = datetime.datetime.now()
             mes_date = day.strftime("%d %B, %Y")
             mes_dept = request.form.get("department_name")
-            # get data from Mongo
+            # get data from users collection in Mongo
             user = mongo.db.users.find_one({"username": session["user"]})
             username = session["user"]
             first = user["firstname"]
@@ -481,15 +499,17 @@ def edit_message(message_id, depart, user):
                 "image_src": request.form.get("image_src"),
                 "image_name": request.form.get("image_name"),
             }
-            # update data in Mongo
+            # update document in selected department collection in Mongo
             mongo.db[mes_dept].update({"_id": ObjectId(message_id)}, edit)
             flash("Message Updated")
+            # Render the Department page
             return redirect(url_for("get_dep", dep=mes_dept))
 
-        # get data from Mongo
+        # get selected document in selected department collection in Mongo
         message = mongo.db[depart].find_one({"_id": ObjectId(message_id)})
-        # get data from Mongo
+        # get depts collection data from Mongo
         depts = mongo.db.depts.find().sort("dept_name", 1)
+        # Render the Edit message page
         return render_template(
             "edit_message.html", message=message, depts=depts)
 
@@ -516,13 +536,15 @@ def edit_image(image_id):
                 "image_des": request.form.get("image_des"),
                 "image_src": request.form.get("image_src")
             }
-            # update data to Mongo
+            # update selected document in images collection in Mongo
             mongo.db.images.update({"_id": ObjectId(image_id)}, edit)
             flash("Image Updated")
+            # Render the Images page
             return redirect(url_for("get_image"))
 
-        # get data from Mongo
+        # get selected document in images collection in Mongo
         image = mongo.db.images.find_one({"_id": ObjectId(image_id)})
+        # Render the Edit Images page
         return render_template("edit_image.html", image=image)
 
 
@@ -546,9 +568,10 @@ def delete_message(message_id, depart, user):
     """
 
     if session["user"] == user or session["user"] == "admin":
-        # remove data from Mongo
+        # remove selected document from selected department collection in Mongo
         mongo.db[depart].remove({"_id": ObjectId(message_id)})
         flash("Message Deleted")
+        # Render the Department page
         return redirect(url_for("get_dep", dep=depart))
 
 
@@ -567,18 +590,20 @@ def add_script(script_id):
     """
 
     if session["user"] == "admin":
-        # get data from Mongo
+        # get the one document from latest_script collection in Mongo
         script = list(mongo.db.latest_script.find())
         if request.method == "POST":
             latest = {
                 "script": request.form.get("script_name")
             }
-            # update data from Mongo
+            # update the singular document in the latest_script collection in Mongo
             mongo.db.latest_script.update(
                 {"_id": ObjectId(script_id)}, latest)
             flash("Script Successfully Updated")
+            # Render the User Home page
             return redirect(url_for("user_home", username=session["user"]))
 
+        # Render the Add Script page
         return render_template("add_script.html", script=script)
 
 
@@ -601,12 +626,14 @@ def add_shot():
             newshot = {
                 "shotlist": request.form.get("shot_name")
             }
-            # gupdateet data to Mongo
+            # update the singular document in the shotlist collection in Mongo
             mongo.db.shotlist.update(
                 {"_id": ObjectId("6029b7f80febec6e0f942fcb")}, newshot)
             flash("Shotlist Successfully Updated")
+            # Render the User Home page
             return redirect(url_for("user_home", username=session["user"]))
 
+        # Render the Add Shotlist page
         return render_template("add_shotlist.html")
 
 
@@ -631,11 +658,13 @@ def add_image():
                 "image_des": request.form.get("image_des"),
                 "image_src": request.form.get("image_src")
             }
-            # update data to Mongo
+            # add a new document to the images collection in Mongo
             mongo.db.images.insert_one(new_image)
             flash("Image Added")
+            # Render the User Home page
             return redirect(url_for("user_home", username=session["user"]))
 
+        # Render the Add Image page
         return render_template("add_image.html")
 
 
@@ -655,33 +684,36 @@ def remove_user():
 
     if session["user"] == "admin":
         if request.method == "POST":
-            # remove data from Mongo
+            # remove document from the users collection in Mongo
             mongo.db.users.remove({"firstname": request.form.get(
                 "first_name"), "lastname": request.form.get("last_name")})
             flash("User Successfully Removed")
+            # Render the User Base page
             return redirect(url_for("user_home", username=session["user"]))
 
+        # Render the Remove User page
         return render_template("remove_user.html")
 
 
 @app.route("/delete_image/<image_id>")
 def delete_image(image_id):
-    """Update the script URL.
+    """Delete an image.
 
-    On Get checks if admin is user and if true renders the Add Script page
-    with the current script URL as a value in the input box.
-    On Post the document is updated in Mongo.
+    On Get checks if admin is user and if true displays a modal asking the
+    user to confirm delete.
+    On confirming the document is deleted in Mongo.
 
-    :param script_id: the selected document to be updated
+    :param image_id: the selected document to be deleted
     :type dep: str
-    :return: edit_message.html
+    :return: user_home.html
     :rtype: n/a
     """
 
     if session["user"] == "admin":
-        # remove data from Mongo
+        # delete document in the images collection in Mongo
         mongo.db.images.delete_one({"_id": ObjectId(image_id)})
         flash("Image Removed")
+        # render Images page
         return redirect(url_for("get_image"))
 
 
